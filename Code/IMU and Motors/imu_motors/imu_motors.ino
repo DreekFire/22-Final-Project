d29a603559a1245edb80b89075ab2c475c20eb46
twaddellberkeley <90221506+twaddellberkeley@users.noreply.github.com>
2022-12-04 16:54:19 -0500

    Update imu_motors.ino
    Fixed motor call, added bluetooth, added input and output for BT, fixed smaller bugs

--- Code/IMU and Motors/imu_motors/imu_motors.ino
+++ Code/IMU and Motors/imu_motors/imu_motors.ino
@@ -1,12 +1,33 @@
 #include "MPU9250.h"
 #include <ESP32Encoder.h>
+#include "BluetoothSerial.h"
 
+//Setting various globals
 int MAX_RPM = 435; // no-load RPM at 12VDC
 int MAX_TORQUE = 18.7; // stall torque at 12VDC, kg * cm
 int TICKS_PER_REV = 384.5;
 
+//Bluetooth input/output vars
+bool motorToggle = false;
+bool writeToggle = false;
+String inputPacket = "";
+String outputPacket = "";
+
+//Motor speeds
+float motorVal[3] = {0.0,0.0,0.0};
+
+//Trying to enable BT
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+//Importing certain vars.
 MPU9250 mpu;
 ESP32Encoder encs[3];
+BluetoothSerial SerialBT;
+
+// Incrimental int to ensure that printing only happens every 256th loop
+int printCount = 0;
 
 // pin config
 struct motor_pin_config {
@@ -17,6 +38,10 @@ struct motor_pin_config {
   int DT;
 };
 
+// Setting pins for each motor
+// pin(0) = IN_1, pin(1) = IN_2
+// pin(2) = PWM, pin(3) = CLK, pin(4) = DT
+// Output on motor from L to R is DT, CLK, GND, V
 struct motor_pin_config motor_cfg[3] = {
   {25, 4, 26, 15, 32},
   {17, 16, 21, 14, 33},
@@ -38,16 +63,18 @@ struct motor motors[3] = {
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+  SerialBT.begin("BallBot"); //Bluetooth device name
   delay(2000);
 
-  if (!mpu.setup(0x68)) {  // change to your own address
+  // Trying to setup IMU on address 0x68
+  if (!mpu.setup(0x68)) {  
       while (1) {
           Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
           delay(5000);
       }
   }
 
-  // calibrate anytime you want to
+  // Calibrating IMU in stationary.
   Serial.println("Accel Gyro calibration will start in 5sec.");
   Serial.println("Please leave the device still on the flat plane.");
   mpu.verbose(true);
@@ -62,6 +89,8 @@ void setup() {
   print_calibration();
   mpu.verbose(false);
   
+  // Looping through Motor pints and setting proper pins. Output is for writing speed values
+  // input is for getting encoder values
   for (int i = 0; i < 3; i++) {
     pinMode(motor_cfg[i].IN_1, OUTPUT);
     pinMode(motor_cfg[i].IN_2, OUTPUT);
@@ -76,6 +105,7 @@ void setup() {
 }
 
 void loop() {
+  // Checking status of IMU, setting data as needed
   if (mpu.update()) {
   static uint32_t prev_ms = millis();
     if (millis() > prev_ms + 25) {
@@ -84,12 +114,59 @@ void loop() {
     }
 
   }
-  print_speeds();
-  // set_voltage(0, 0.15);
-  // set_voltage(1, 0.15);
-  // set_voltage(2, 0.15);
+  // Checking if it is time to print, if not we let BT have the serial for recieving commands
+  if (printCount >= 256){
+    print_speeds();
+    printCount = 0;
+    // Send routine output packet
+    outputPacket = String(mpu.getYaw()) +"," + String(mpu.getPitch()) + "," + String(mpu.getRoll());
+
+  }
+  // Recieve BT commands
+  else{
+    //Writing to BT out
+    if (outputPacket != "") {
+      SerialBT.flush();
+      SerialBT.println(outputPacket);
+    }
+    //Recieving to BT in
+    if (SerialBT.available()) {
+      inputPacket = SerialBT.readStringUntil('\n');
+      Serial.println(inputPacket);
+      inputPacket.trim();
+    }
+    //Read char e for toggle motors
+    if (inputPacket == "e"){
+      motorToggle = !motorToggle;
+      Serial.print("TOGGLE");
+    }
+    //Clearing packets
+    inputPacket = "";
+    outputPacket = "";
+  }
+
+  //This line needed to ensure that the IMU data is up to date
+  motorVal[0] = get_speed(0);
+  motorVal[1] = get_speed(1);
+  motorVal[2] = get_speed(2);
+  
+  printCount++;
+
+  // Motor speed set to different voltages, toggled via variable
+  if (motorToggle){
+    set_voltage(0, 0.15);
+    set_voltage(1, 0.15);
+    set_voltage(2, 0.15);
+  }
+  else{
+    set_voltage(0, 0);
+    set_voltage(1, 0);
+    set_voltage(2, 0);
+  }
+
 }
 
+//Method to print out IMU data
 void print_roll_pitch_yaw() {
   Serial.print("Yaw, Pitch, Roll: ");
   Serial.print(mpu.getYaw(), 2);
@@ -99,6 +176,7 @@ void print_roll_pitch_yaw() {
   Serial.println(mpu.getRoll(), 2);
 }
 
+//Method to calibrate IMU
 void print_calibration() {
   Serial.println("< calibration parameters >");
   Serial.println("accel bias [g]: ");
@@ -131,6 +209,7 @@ void print_calibration() {
   Serial.println();*/
 }
 
+// Method to set motor speed
 // motor_id: 0 to 2
 // voltage: -1.0 to 1.0
 void set_voltage(int motor_id, float voltage) {
@@ -149,6 +228,7 @@ void set_torque(int motor_id, float torque) {
   set_voltage(motor_id, voltage);
 }
 
+// Method to read encoders
 // motor_id: 0 to 2
 // returns speed in RPM
 float get_speed(int motor_id) {
@@ -167,12 +247,13 @@ float get_speed(int motor_id) {
   return filtered_vel / TICKS_PER_REV * 60;
 }
 
+//Method to print each motor speed
 void print_speeds() {
   Serial.println("< motor speeds >");
-  Serial.print(get_speed(0));
+  Serial.print(motorVal[0]);
   Serial.print(", ");
-  Serial.print(get_speed(1));
+  Serial.print(motorVal[1]);
   Serial.print(", ");
-  Serial.print(get_speed(2));
+  Serial.print(motorVal[2]);
   Serial.println();
 }
